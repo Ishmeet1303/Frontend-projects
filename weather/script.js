072391ca8b34e754d22083f81b6e758661fb7134
Ishmeet1303 <ishmeetsingh9670@gmail.com>
2025-09-15 11:27:11 +0530

    files added

--- weather/script.js
+++ weather/script.js
@@ -2,7 +2,7 @@
 
 const temperatureField=document.querySelector(".temp")
 const locationField=document.querySelector(".time_location p")
-const dateandTimeField=document.querySelector(".temp_location span")
+const dateandTimeField=document.querySelector(".time_location span")
 const conditionField=document.querySelector(".condition p ")
 const searchField=document.querySelector(".search_location")
 const form=document.querySelector('form')
@@ -20,7 +20,7 @@ const fetchresults = async (targetLocation) =>{
 
     let locationName=data.location.name
     let time=data.location.localtime
-
+    console.log(time)
     let temp=data.current.temp_c
     let condition=data.current.condition.text
     updateDetails(temp,locationName,time,condition)
@@ -29,16 +29,11 @@ const fetchresults = async (targetLocation) =>{
 
 //function to update
 function updateDetails(temp,locationName,time,condition){
-    let splitDate=time.split(' ')[0]
-
-    let splitTime=time.split(' ')[1]
-
-    let currentDay = getdayName(new Date(splitDate).getDay())
   
   
     temperatureField.innerText=temp
     locationField.innerText=locationName
-    dateandTimeField.innerText=`${splitDate} ${currentDay} ${splitTime}`
+    dateandTimeField.innerText=time
     conditionField.innerText=condition
 
 
@@ -53,22 +48,3 @@ function searchForLocation(e){
 
 searchForLocation()
 fetchresults(target)
-
-function getdayName(number){
-    switch(number){
-        case 0:
-            return "Sunday"
-            case 1:
-            return "Monday"
-            case 2:
-            return "Tuesday"
-            case 3:
-            return "Wednesday"
-            case 4:
-            return "Thrusday"
-            case 5:
-            return "Friday"
-            case 6:
-            return "Saturday"
-    }
-}
